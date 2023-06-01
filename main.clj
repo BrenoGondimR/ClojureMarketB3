4904b66e5e31463613c3909e2bfe6e343a5fa124
Breno Gondim <breno_gondim@hotmail.com>
2023-05-31 22:23:36 -0300

    ajuste formatacao json

--- main.clj
+++ main.clj
@@ -44,8 +44,40 @@
 
 (defn print-api-data [stock-code]
   (println "Obtendo detalhes da ação...")
-  (let [api-url (str "https://brapi.dev/api/quote/" stock-code)]
-    (print-json-from-api api-url)))
+  (let [api-url (str "https://brapi.dev/api/quote/" stock-code)
+        response (http/get api-url)
+        body (json/read-str (:body response) :key-fn keyword)]
+    (if (= (:status response) 200)
+      (let [results (get body :results)
+            parsed-body (if (not-empty results) (first results))]
+        (if parsed-body
+          (do
+            (println "Detalhes da ação:")
+            (let [short-name (get parsed-body :shortName)
+                  long-name (get parsed-body :longName)
+                  symbol (get parsed-body :symbol)
+                  asset-type (or (re-find #"ON" short-name) (re-find #"PN" short-name) "Não disponível")
+                  variation (str (get parsed-body :regularMarketChange) " (" (get parsed-body :regularMarketChangePercent) "%)")
+                  last-price (get parsed-body :regularMarketPrice)
+                  high (get parsed-body :regularMarketDayHigh)
+                  low (get parsed-body :regularMarketDayLow)
+                  open (get parsed-body :regularMarketOpen)
+                  close (get parsed-body :regularMarketPreviousClose)
+                  time (get parsed-body :regularMarketTime)]
+              (println "Nome da ação: " (if short-name (first (str/split short-name #" ")) "") " (" long-name ")")
+              (println "Código da ação: " symbol)
+              (println "Tipo de ativo: " asset-type)
+              (println "Descrição da ação: Não há um campo específico para a descrição da ação neste JSON.")
+              (println "Variação do dia (em R$): " (get parsed-body :regularMarketChange))
+              (println "Variação do dia (percentual): " (get parsed-body :regularMarketChangePercent) "%")
+              (println "Último preço: " last-price)
+              (println "Preço máximo: " high)
+              (println "Preço mínimo: " low)
+              (println "Preço de abertura: " open)
+              (println "Preço de fechamento: " close)
+              (println "Hora: " time))))
+        (println "Nenhum resultado encontrado para a ação.")))
+    (println "Erro na conexão com a API.")))
 
 (defn -main []
   (let [csv-data (csv->maps "acoes-listadas.csv")
--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
                  [org.clojure/data.csv "1.0.0"]
                  [org.clojure/data.json "1.0.0"]
                  [clj-http "3.11.0"]
-                 [http-kit "2.4.0"]])
+                 [http-kit "2.4.0"]
+                 [cheshire "5.13.1"]])
 
